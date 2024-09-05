e19b824724584ce74a64d4a40ee77a57d9e449a7
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-09-05 15:43:28 +0200

    Update exports

--- build.sh
+++ build.sh
@@ -1,2 +1,22 @@
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.fdb_latexmk
+rm *.fls
+rm *.log
+rm *.out
+
+rm *.pdf
 python src/resumator/make_cv.py
-latexmk -pdf -interaction=nonstopmode -pvc cv
+pdflatex -interaction=nonstopmode cv.tex
+pdflatex -interaction=nonstopmode main.tex
+pdflatex -interaction=nonstopmode funding.tex
+pdflatex -interaction=nonstopmode publications.tex
+
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.fdb_latexmk
+rm *.fls
+rm *.log
+rm *.out
